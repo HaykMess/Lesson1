3e1f996385d16e82c3c0fcc646f09728443e1e2a
HaykMess <haykmess@gmail.com>
2024-03-23 23:21:33 +0400

    My first commit

---
+++ Lesson1/src/Main.java
@@ -0,0 +1,30 @@
+class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Barev1");
+        int x = 70;
+        char d = ('.');
+        System.out.println(x);
+        System.out.println(d);
+        float f = 0.1F;
+        double q = 45.6;
+        System.out.println(f);
+        System.out.println(q);
+        boolean e = true;
+        boolean t = false;
+        System.out.println(e);
+        System.out.println(t);
+        String TheEnd = "The end Verj" + " " + q;
+        System.out.println(TheEnd);
+        byte u = 4;
+        System.out.println(u);
+        short l = 111;
+        u = (byte) l;
+        System.out.println(u);
+        String s = "2";
+        System.out.println(s);
+        int m = Byte.parseByte  (s);
+
+        System.out.println(m+1);
+    }
+}
