94403d420edf68d967069c3e697dfea1becb1bfa
HaykMess <haykmess@gmail.com>
2024-03-24 01:27:12 +0400

    change 2 commit

---
+++ Lesson2/src/Operations.java
@@ -0,0 +1,21 @@
+public class Operations {
+    public static void main(String[] args) {
+        int x;
+        int $;
+        int _x = 4;
+        System.out.println(_x);
+
+        int a = 4, b = 5; // test
+        int c = a + b;
+        System.out.println(c);
+        System.out.println(c/b); //qani hat c ka b i mej
+        System.out.println(c%b); //mnacord
+        double t = 10.0, g = 3.0;
+        System.out.println(t/g); // dabli depqum kstananq normal bajanum
+
+
+
+
+
+    }
+}
